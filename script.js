ea8e178b5bf89170ae7c7db735752d1e8e7903b4
gen1chiro <juljavellana1218@gmail.com>
2024-11-15 16:57:59 +0800

    Refactor functions

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
-const cashDisplay = document.querySelector(".cid");
-let cash = 5;
+const changeDisplay = document.querySelector(".cid");
+const totalDisplay = document.getElementById("total");
+const cashDisplay = document.getElementById("cash");
+const changeDueDisplay = document.getElementById("change-due");
+const inputButtons = document.querySelectorAll(".button");
+const clearButton = document.querySelector(".clear-button");
+const purchaseButton = document.getElementById("purchase-btn");
 let price = 3.26;
+
 let cid = [
     ['PENNY', 1.01],
     ['NICKEL', 2.05],
@@ -25,33 +31,107 @@ const denominations = [
 ];
 
 const cidMap = new Map(cid);
-let change = cash - price;
-//change = parseFloat(change.toFixed(2));
-const result = [];
 
-denominations.forEach(denomination => {
-    let availableCash = cidMap.get(denomination.name);
-    let count = 0;
+const cidTotal = () => Array.from(cidMap.values()).reduce((a, c) => a + c, 0);
 
-    while (change >= denomination.value && availableCash >= denomination.value) {
-        change -= denomination.value;
-        //change = parseFloat(change.toFixed(2));
-        availableCash -= denomination.value;
-        count++;
+const checkCustomerCash = () => Number(cashDisplay.value) >= price;
+
+const calculateChange = () => {
+    const result = [];
+    let status = "Status: OPEN";
+    let cidValues = [];
+    let change = Number(cashDisplay.value) - price;
+    change = parseFloat(change.toFixed(2));
+
+    if (change === 0.00) {
+        updateChangeDueDisplay(result, "No change due - customer paid with exact cash");
+        return;
     }
 
-    cidMap.set(denomination.name, availableCash);
-
-    if (count > 0) {
-        result.push([denomination.name, denomination.value * count]);
+    if (change > cidTotal()) {
+        updateChangeDueDisplay(result, "Status: INSUFFICIENT_FUNDS");
+        return;
     }
+
+    denominations.forEach(denomination => {
+        let availableCash = cidMap.get(denomination.name);
+        let count = 0;
+
+        while (change >= denomination.value && availableCash >= denomination.value) {
+            change -= denomination.value;
+            change = parseFloat(change.toFixed(2));
+            availableCash -= denomination.value;
+            availableCash = parseFloat(availableCash.toFixed(2));
+            count++;
+        }
+
+        cidMap.set(denomination.name, availableCash);
+
+        if (count > 0) {
+            result.push([denomination.name, denomination.value * count]);
+        }
+    });
+
+    if (cidTotal() === 0) {
+        status = "Status: CLOSED";
+    }
+
+    updateChangeDueDisplay(result, status);
+}
+
+const updateCidDisplay = () => {
+    changeDisplay.innerHTML = "<p class=\"cid-header\"><strong>Change in Drawer</strong></p>";
+    cidMap.forEach((value, key) => {
+        const paragraph = document.createElement("p");
+        paragraph.textContent = `${key}: ${value}`;
+        changeDisplay.appendChild(paragraph);
+    });
+}
+
+const updateChangeDueDisplay = (result, status) => {
+    changeDueDisplay.innerHTML = `<p>${status}</p>`;
+    result.forEach(result => {
+        const paragraph = document.createElement("p");
+        paragraph.textContent = `${result[0]}: ${result[1]}`;
+        changeDueDisplay.appendChild(paragraph);
+    })
+}
+
+const clearCashDisplay = () => {
+    cashDisplay.value = "";
+}
+
+const displayTotal = () => {
+    totalDisplay.value = price;
+}
+
+updateCidDisplay();
+displayTotal();
+
+purchaseButton.addEventListener("click", () => {
+    if (!checkCustomerCash()) {
+        alert("Customer does not have enough money to purchase the item");
+        clearCashDisplay();
+        return;
+    }
+    calculateChange();
+    updateCidDisplay();
+    clearCashDisplay();
 });
-console.log(cidMap);
-console.log(result);
 
-cidMap.forEach((value, key) => {
-    const paragraph = document.createElement("p");
-    paragraph.textContent = `${value}: ${key}`;
-    cashDisplay.appendChild(paragraph);
-})
+inputButtons.forEach(button => {
+    button.addEventListener("click", () => {
+        if (button.value === "." && cashDisplay.value.includes(".")) {
+            return;
+        }
+        if (button.value === "0" && cashDisplay.value === "") {
+            return;
+        }
+        cashDisplay.value += button.value;
+    });
+});
+
+clearButton.addEventListener("click", () => {
+    cashDisplay.value = "";
+});
 
